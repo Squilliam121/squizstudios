0bb8dd42380c68b6d51bd4822b25f3f8c04cef08
William Simpson <squilliam121@gmail.com>
2025-05-07 15:00:13 +1200

    message

--- hungergames/index.js
+++ hungergames/index.js
@@ -1,4 +1,4 @@
 const bloodbathEntries = {
-    entry: ["eee","eeeeee"],
-    people: ["1","3"],
+    entry: ["PERSON1 run away from the cornucopia.","PERSON1 fought PERSON2, but wasn't successful with murder"],
+    people: ["1","2"],
 }
