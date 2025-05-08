108e8456b5f4a4e7e37d26ad480395a3b1fe0619
William Simpson <squilliam121@gmail.com>
2025-05-08 20:51:57 +1200

    finalUpdate

--- hungergames/index.js
+++ hungergames/index.js
@@ -1,29 +1,259 @@
 const heading = document.getElementById("day")
 const events = document.getElementById("events")
 const bottom = document.getElementById("bottomtext")
+const content = document.getElementById("content")
+
+const d1m = document.getElementById("d1m")
+const d2m = document.getElementById("d2m")
+const d3m = document.getElementById("d3m")
+const d4m = document.getElementById("d4m")
+const d5m = document.getElementById("d5m")
+const d6m = document.getElementById("d6m")
+const d7m = document.getElementById("d7m")
+const d8m = document.getElementById("d8m")
+const d9m = document.getElementById("d9m")
+const d10m = document.getElementById("d10m")
+const d11m = document.getElementById("d11m")
+const d12m = document.getElementById("d12m")
+
+const d1f = document.getElementById("d1f")
+const d2f = document.getElementById("d2f")
+const d3f = document.getElementById("d3f")
+const d4f = document.getElementById("d4f")
+const d5f = document.getElementById("d5f")
+const d6f = document.getElementById("d6f")
+const d7f = document.getElementById("d7f")
+const d8f = document.getElementById("d8f")
+const d9f = document.getElementById("d9f")
+const d10f = document.getElementById("d10f")
+const d11f = document.getElementById("d11f")
+const d12f = document.getElementById("d12f")
 
 let stage = "reaping"
 
 document.getElementById("proceed-btn").addEventListener("click", newpage);
 
-function newpage() {
-    clear();
+d1m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d1m.value !== "" && d1m.readOnly === false) {
+        d1m.readOnly = true;
+        d1m.style.color = "#c6c6c6"
+        startingtributes.push(d1m.value)
+    }
+})
 
-    if (stage === "reaping"){
+d2m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d2m.value !== "" && d2m.readOnly === false) {
+        d2m.readOnly = true;
+        d2m.style.color = "#c6c6c6"
+        startingtributes.push(d2m.value)
+    }
+})
+
+d3m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d3m.value !== "" && d3m.readOnly === false) {
+        d3m.readOnly = true;
+        d3m.style.color = "#c6c6c6"
+        startingtributes.push(d3m.value)
+    }
+})
+
+d4m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d4m.value !== "" && d4m.readOnly === false) {
+        d4m.readOnly = true;
+        d4m.style.color = "#c6c6c6"
+        startingtributes.push(d4m.value)
+    }
+})
+
+d5m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d5m.value !== "" && d5m.readOnly === false) {
+        d5m.readOnly = true;
+        d5m.style.color = "#c6c6c6"
+        startingtributes.push(d5m.value)
+    }
+})
+
+d6m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d6m.value !== "" && d6m.readOnly === false) {
+        d6m.readOnly = true;
+        d6m.style.color = "#c6c6c6"
+        startingtributes.push(d6m.value)
+    }
+})
+
+d7m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d7m.value !== "" && d7m.readOnly === false) {
+        d7m.readOnly = true;
+        d7m.style.color = "#c6c6c6"
+        startingtributes.push(d7m.value)
+    }
+})
+
+d8m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d8m.value !== "" && d8m.readOnly === false) {
+        d8m.readOnly = true;
+        d8m.style.color = "#c6c6c6"
+        startingtributes.push(d8m.value)
+    }
+})
+
+d9m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d9m.value !== "" && d9m.readOnly === false) {
+        d9m.readOnly = true;
+        d9m.style.color = "#c6c6c6"
+        startingtributes.push(d9m.value)
+    }
+})
+
+d10m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d10m.value !== "" && d10m.readOnly === false) {
+        d10m.readOnly = true;
+        d10m.style.color = "#c6c6c6"
+        startingtributes.push(d10m.value)
+    }
+})
+
+d11m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d11m.value !== "" && d11m.readOnly === false) {
+        d11m.readOnly = true;
+        d11m.style.color = "#c6c6c6"
+        startingtributes.push(d11m.value)
+    }
+})
+
+d12m.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d12m.value !== "" && d12m.readOnly === false) {
+        d12m.readOnly = true;
+        d12m.style.color = "#c6c6c6"
+        startingtributes.push(d12m.value)
+    }
+})
+
+d1f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d1f.value !== "" && d1f.readOnly === false) {
+        d1f.readOnly = true;
+        d1f.style.color = "#c6c6c6"
+        startingtributes.push(d1f.value)
+    }
+})
+
+d2f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d2f.value !== "" && d2f.readOnly === false) {
+        d2f.readOnly = true;
+        d2f.style.color = "#c6c6c6"
+        startingtributes.push(d2f.value)
+    }
+})
+
+d3f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d3f.value !== "" && d3f.readOnly === false) {
+        d3f.readOnly = true;
+        d3f.style.color = "#c6c6c6"
+        startingtributes.push(d3f.value)
+    }
+})
+
+d4f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d4f.value !== "" && d4f.readOnly === false) {
+        d4f.readOnly = true;
+        d4f.style.color = "#c6c6c6"
+        startingtributes.push(d4f.value)
+    }
+})
+
+d5f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d5f.value !== "" && d5f.readOnly === false) {
+        d5f.readOnly = true;
+        d5f.style.color = "#c6c6c6"
+        startingtributes.push(d5f.value)
+    }
+})
+
+d6f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d6f.value !== "" && d6f.readOnly === false) {
+        d6f.readOnly = true;
+        d6f.style.color = "#c6c6c6"
+        startingtributes.push(d6f.value)
+    }
+})
+
+d7f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d7f.value !== "" && d7f.readOnly === false) {
+        d7f.readOnly = true;
+        d7f.style.color = "#c6c6c6"
+        startingtributes.push(d7f.value)
+    }
+})
+
+d8f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d8f.value !== "" && d8f.readOnly === false) {
+        d8f.readOnly = true;
+        d8f.style.color = "#c6c6c6"
+        startingtributes.push(d8f.value)
+    }
+})
+
+d9f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d9f.value !== "" && d9f.readOnly === false) {
+        d9f.readOnly = true;
+        d9f.style.color = "#c6c6c6"
+        startingtributes.push(d9f.value)
+    }
+})
+
+d10f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d10f.value !== "" && d10f.readOnly === false) {
+        d10f.readOnly = true;
+        d10f.style.color = "#c6c6c6"
+        startingtributes.push(d10f.value)
+    }
+})
+
+d11f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d11f.value !== "" && d11f.readOnly === false) {
+        d11f.readOnly = true;
+        d11f.style.color = "#c6c6c6"
+        startingtributes.push(d11f.value)
+    }
+})
+
+d12f.addEventListener("keydown", function(event) {
+    if (event.key === "Enter" && d12f.value !== "" && d12f.readOnly === false) {
+        d12f.readOnly = true;
+        d12f.style.color = "#c6c6c6"
+        startingtributes.push(d12f.value)
+    }
+})
+
+async function newpage() {
+    console.log(startingtributes.length)
+    if (stage === "reaping" && startingtributes.length === 24){
+        clear();
         stage = "bloodbath"
+        content.style.display = "none"
+        tributes = startingtributes
+        tributecount = tributes.length
         bloodBath()
         return
+    }else if (tributecount === 1){
+        clear();
+        document.getElementById("proceed-btn").style.display = "none"
+        heading.innerText = "Winner"
+        bottom.innerHTML = `<span class="highlight">${tributes[0]}</span> has won the Hunger Games!`
     }else if (stage === "bloodbath"){
+        clear();
         stage = "day1"
         day()
         return
     }else if (stage.includes("night")){
+        clear();
         const stagenum = stage.slice(5);
         stage = "day" + (parseInt(stagenum)+1) 
         console.log(stage)
         day()
         return
-    }else{
+    }else if (stage.includes("day")){
+        clear();
         const stagenum = stage.slice(3);
         stage = "night" + (parseInt(stagenum)) 
         console.log(stage)
@@ -119,7 +349,7 @@ const nightEntries = {
     people: ["1","1","2","1","2","3","4","3","2","2","2","2","1","3","2","1","2","2"],
 }
 
-let startingtributes = ["Gabe","William","Pabbles","Dash","EJ","Eli","Storm","Mitchell","Jambo","Jam","Ollie","Otherwilliam","Toby","Devhan","Louis","Keaton","Matthew","Lachie","Pharaoh","Harry","Sean","Zab","Gav","Jack"]
+let startingtributes = []
 let tributes = startingtributes
 let tributecount = tributes.length
 let eventslist = []
