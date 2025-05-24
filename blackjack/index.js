b857b56649ac0337c131ec988a1f6fcb164c8289
squilliam121 <squilliam121@gmail.com>
2025-05-24 12:51:20 +1200

    blackJack

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,16 +1,16 @@
 const cards = document.getElementsByClassName('card');
 const carddiv = document.getElementById('cards')
 const actionbar = document.getElementById('actionbar')
+const headings = document.getElementsByClassName('default-text')
+const input = document.getElementById('startinput')
+const invisibles = document.querySelectorAll('.invisible');
+const start = document.getElementById('start')
+const highscore = document.getElementById('highscore')
 
-let player1cards = {
-    number: [],
-    house: []
-}
-
-let player2cards = {
-    number: [],
-    house: []
-}
+let scores = [0]
+let players = 0
+let currentplayer = 1
+let currenthighscore = 0
 
 const images = [
     "images/spade.png",
@@ -19,7 +19,24 @@ const images = [
     "images/heart.png",
 ]
 
-function newCardAppend(){
+function winnerNow() {
+    for (let i = 0; i < scores.length; i++){
+        if (scores[i] >= currenthighscore){
+            currenthighscore = scores[i]
+        }
+    }
+    clearCards()
+    for (let i = 0; i < headings.length; i++) {
+        headings[i].classList.add("invisible")
+    }
+    highscore.classList.add('invisible')
+    start.classList.remove('invisible')
+    actionBarClose()
+    let winner = scores.findIndex(item => item === currenthighscore);
+    start.innerText = `Player ${winner+1} has won!`
+}
+
+async function newCardAppend(){
     const newCard = document.createElement('div')
     newCard.classList.add('card')
 
@@ -54,8 +71,60 @@ function newCardAppend(){
     newCard.appendChild(mimg);
 
     carddiv.appendChild(newCard);
-
+    scores[currentplayer-1] += cardnum
     newCard.style.animation = `flyIn 0.4s ease-out forwards`;
+    if (scores[currentplayer-1] > 21) {
+        scores[currentplayer-1] = 0
+        nextPlayer()
+        return
+    }else if (scores[currentplayer-1] == 21) {
+        winnerNow()
+        return
+    }
+}
+
+function clearCards() {
+    while (carddiv.firstChild) {
+        carddiv.removeChild(carddiv.firstChild);
+    }
+}
+
+function changeHeadings() {
+    for (let i = 0; i < headings.length; i++) {
+        headings[i].innerText = `Player ${currentplayer}`;
+    }
+}
+
+function nextPlayer() {
+    if (currentplayer < players){
+        scores.push(0)
+        currentplayer += 1
+        for (let i = 0; i < scores.length; i++){
+            if (scores[i] >= currenthighscore){
+                currenthighscore = scores[i]
+            }
+        }
+        clearCards()
+        changeHeadings()
+        newCardAppend()
+        newCardAppend()
+        highscore.innerHTML = `Highscore: ${currenthighscore}`
+    }else if (currentplayer >= players){
+        for (let i = 0; i < scores.length; i++){
+            if (scores[i] >= currenthighscore){
+                currenthighscore = scores[i]
+            }
+        }
+        clearCards()
+        for (let i = 0; i < headings.length; i++) {
+            headings[i].classList.add("invisible")
+        }
+        highscore.classList.add('invisible')
+        start.classList.remove('invisible')
+        actionBarClose()
+        let winner = scores.findIndex(item => item === currenthighscore);
+        start.innerText = `Player ${winner+1} has won!`
+    }
 }
 
 function actionBar() {
@@ -65,11 +134,6 @@ function actionBar() {
 function actionBarClose() {
     actionbar.classList.remove('active')
 }
-
-newCardAppend()
-newCardAppend()
-newCardAppend()
-newCardAppend()
 
 const cardsArray = Array.from(cards); // convert HTMLCollection to real array
 cardsArray.forEach((card, index) => {
@@ -85,3 +149,17 @@ cardsArray.forEach((card, index) => {
     bimg.src = images[cardimg]
     mimg.src = images[cardimg]
 });
+
+input.addEventListener('keydown', (event)=>{
+    if (event.key === "Enter" && !isNaN(input.value)){
+        document.body.classList.remove('p1')
+        invisibles.forEach(el => {
+            el.classList.remove('invisible');
+        });
+        input.classList.add('invisible')
+        start.classList.add('invisible')
+        players = input.value
+        newCardAppend()
+        newCardAppend()
+    }
+})
